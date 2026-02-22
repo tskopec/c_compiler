ce2caeecbe9475b4408703b937996bb2700b710c
tskopec <tskopec@gmail.com>
2026-02-22 23:32:03 +0100

    uniq error code for each phase

--- src/com.pl
+++ src/com.pl
@@ -16,17 +16,17 @@ use Emitter;
 
 
 our $global_counter = 0;
+my $error_code = 255;
 
 # ARGS
 our %debug;
-my @src_files;
+my $src_file;
 my $target_phase = "";
 my $dont_link = 0;
 
-
 foreach (@ARGV) {
 	if (/\.c$/) {
-		push @src_files, $_;
+		$src_file = $_;
 	} elsif (/^--(lex|parse|validate|tac|codegen)$/) {
 		$target_phase = $1;
 	} elsif (/^-d(\w*)$/) {
@@ -36,73 +36,65 @@ foreach (@ARGV) {
 	}
 }
 
-for my $src_file (@src_files) {
-	eval { compile($src_file) };
-	if ($@) {
-		print "ERROR in src file: $src_file:\n$@";
-	} else {
-		say "OK, src file: $src_file";
-	}
-	say "------------------------------------------";
-} continue {
-	$global_counter = 0;
+# PREPROCESS
+my $prep_file = $src_file =~ s/c$/i/r;
+qx/gcc -E -P $src_file -o $prep_file/;
+
+# LEX
+$error_code = 1;
+my $src_str = read_file($prep_file);
+say "COMPILING $src_file:\n $src_str\n" if (grep { $_ } (values %debug));
+unlink($prep_file); 
+my @tokens = Lexer::tokenize($src_str);
+say(join("\n", @tokens) . "\n") if $debug{l};
+return if ($target_phase eq 'lex'); 	
+
+# PARSE
+$error_code = 2;
+my $ast = Parser::parse(@tokens);
+print_tree($ast) if $debug{p};
+return if ($target_phase eq 'parse');
+
+# SEMANTICS
+$error_code = 3;
+Semantics::run($ast);
+print_tree($ast) if $debug{s};
+return if ($target_phase eq 'validate');
+
+# TAC
+$error_code = 4;
+my $tac = TAC::emit_TAC($ast);
+print_tree($tac) if $debug{t};
+return if ($target_phase eq 'tac');
+
+# ASSEMBLY GEN
+$error_code = 5;
+my $asm = CodeGen::generate($tac);
+print_tree($asm) if $debug{c};
+return if ($target_phase eq 'codegen');
+
+# EMIT CODE
+$error_code = 6;
+my $asm_file = $src_file =~ s/c$/s/r;
+my $code = Emitter::emit_code($asm);
+say($code) if $debug{e};
+write_file($asm_file, $code);
+
+# ASSEMBLE
+$error_code = 7;
+if ($dont_link) {
+	my $obj_file = $src_file =~ s/\.c$/.o/r;
+	qx/gcc -c $asm_file -o $obj_file/;
+} else {
+	my $bin_file = $src_file =~ s/\.c$//r;
+	qx/gcc $asm_file -o $bin_file/;
 }
- 
+unlink($asm_file); 
+
+$error_code = 0;
 
 END {
-	say "compiler done";
+	exit $error_code;
 }
-
-sub compile {
-	my $src_file = shift;
-	# PREPROCESS
-	my $prep_file = $src_file =~ s/c$/i/r;
-	qx/gcc -E -P $src_file -o $prep_file/;
-
-	# LEX
-	my $src_str = read_file($prep_file);
-	say "COMPILING $src_file:\n $src_str\n" if (grep { $_ } (values %debug));
-	unlink($prep_file); 
-	my @tokens = Lexer::tokenize($src_str);
-	say(join("\n", @tokens) . "\n") if $debug{l};
-	return if ($target_phase eq 'lex'); 	
-
-	# PARSE
-	my $ast = Parser::parse(@tokens);
-	print_tree($ast) if $debug{p};
-	return if ($target_phase eq 'parse');
-	
-	# SEMANTICS
-	Semantics::run($ast);
-	print_tree($ast) if $debug{s};
-	return if ($target_phase eq 'validate');
-	
-	# TAC
-	my $tac = TAC::emit_TAC($ast);
-	print_tree($tac) if $debug{t};
-	return if ($target_phase eq 'tac');
-
-	# ASSEMBLY GEN
-	my $asm = CodeGen::generate($tac);
-	print_tree($asm) if $debug{c};
-	return if ($target_phase eq 'codegen');
-	
-	# EMIT CODE
-	my $asm_file = $src_file =~ s/c$/s/r;
-	my $code = Emitter::emit_code($asm);
-	say($code) if $debug{e};
-	write_file($asm_file, $code);
-	  
-	# ASSEMBLE
-	if ($dont_link) {
-		my $obj_file = $src_file =~ s/\.c$/.o/r;
-	 	qx/gcc -c $asm_file -o $obj_file/;
-	} else {
-		my $bin_file = $src_file =~ s/\.c$//r;
-		qx/gcc $asm_file -o $bin_file/;
-	}
-	unlink($asm_file); 
-}
-
 
 
